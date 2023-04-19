bd6d30a54d2eebb13d0c116b9ed53a73830f36d4
fnmgithub <62976502+fnmgithub@users.noreply.github.com>
2023-04-19 23:59:35 +0900

    prettierrc

--- src/main.ts
+++ src/main.ts
@@ -6,42 +6,42 @@ import {
   Partials,
   SlashCommandBuilder,
   AutocompleteInteraction,
-  CommandInteraction,
-} from "discord.js";
-import dotenv from "dotenv";
-import { GuildQueue, Player, QueryType, QueueRepeatMode } from "discord-player";
+  CommandInteraction
+} from 'discord.js';
+import dotenv from 'dotenv';
+import { GuildQueue, Player, QueryType, QueueRepeatMode } from 'discord-player';
 
 dotenv.config();
 
 const commands = [
   new SlashCommandBuilder()
-    .setName("play")
-    .setDescription("play music")
-    .addStringOption((option) =>
+    .setName('play')
+    .setDescription('play music')
+    .addStringOption(option =>
       option
-        .setName("keyword")
-        .setDescription("keyword or url")
+        .setName('keyword')
+        .setDescription('keyword or url')
         .setRequired(true)
     ),
-  new SlashCommandBuilder().setName("next").setDescription("play next track"),
+  new SlashCommandBuilder().setName('next').setDescription('play next track'),
   new SlashCommandBuilder()
-    .setName("disconnect")
-    .setDescription("disconnect this voice channel"),
+    .setName('disconnect')
+    .setDescription('disconnect this voice channel'),
   new SlashCommandBuilder()
-    .setName("repeat")
-    .setDescription("change repeatMode"),
-  new SlashCommandBuilder().setName("list").setDescription("show status"),
-].map((command) => command.toJSON());
+    .setName('repeat')
+    .setDescription('change repeatMode'),
+  new SlashCommandBuilder().setName('list').setDescription('show status')
+].map(command => command.toJSON());
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN!);
 
 (async () => {
   try {
-    console.log("Started refreshing application (/) commands.");
+    console.log('Started refreshing application (/) commands.');
     await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), {
-      body: commands,
+      body: commands
     });
-    console.log("Successfully reloaded application (/) commands.");
+    console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
   }
@@ -66,17 +66,17 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildScheduledEvents,
     GatewayIntentBits.AutoModerationConfiguration,
-    GatewayIntentBits.AutoModerationExecution,
+    GatewayIntentBits.AutoModerationExecution
   ],
-  partials: [Partials.Message, Partials.Channel],
+  partials: [Partials.Message, Partials.Channel]
 });
 
 const player = new Player(client);
 
-client.on("interactionCreate", async (interaction) => {
+client.on('interactionCreate', async interaction => {
   var autocompleteInteraction = interaction as AutocompleteInteraction;
   var commandInteraction = interaction as CommandInteraction;
-  var url = autocompleteInteraction.options.getString("keyword") ?? "not found";
+  var url = autocompleteInteraction.options.getString('keyword') ?? 'not found';
   if (url.match(/https:\/\//)) url = `"${url}"`;
   await commandInteraction.deferReply();
 
@@ -85,28 +85,28 @@ client.on("interactionCreate", async (interaction) => {
     {
       volume: 10,
       metadata: {
-        channel: interaction.channel,
-      },
+        channel: interaction.channel
+      }
     }
   );
 
   switch (autocompleteInteraction.commandName) {
-    case "play":
+    case 'play':
       const track = await player
         .search(url, {
           requestedBy: interaction.user,
           searchEngine: url.match(/https:\/\//)
             ? QueryType.AUTO
-            : QueryType.YOUTUBE_SEARCH,
+            : QueryType.YOUTUBE_SEARCH
         })
-        .then((x) => x.tracks);
+        .then(x => x.tracks);
 
       queue.addTrack(track);
       if (!queue.isPlaying()) {
         try {
           await queue.connect(autocompleteInteraction.channelId);
         } catch (e) {
-          console.log("ボイスチャンネルに参加できませんでした");
+          console.log('ボイスチャンネルに参加できませんでした');
           console.log(e);
         }
         await queue.node.play();
@@ -117,42 +117,42 @@ client.on("interactionCreate", async (interaction) => {
               if (queue.deleted) {
                 clearInterval(interval);
                 setTimeout(() => {
-                  commandInteraction.editReply("finished");
+                  commandInteraction.editReply('finished');
                   return;
                 }, 2000);
               }
             }, 2000);
           }
           commandInteraction.editReply(
-            "Author: " +
+            'Author: ' +
               queue.currentTrack?.author +
-              "\n" +
-              "Title: " +
+              '\n' +
+              'Title: ' +
               queue.currentTrack?.title +
-              "\n" +
-              "Url: " +
-              "<" +
+              '\n' +
+              'Url: ' +
+              '<' +
               queue.currentTrack?.url +
-              ">" +
-              "\n" +
-              "RepeatMode: " +
+              '>' +
+              '\n' +
+              'RepeatMode: ' +
               QueueRepeatMode[queue.node.queue.repeatMode] +
-              "\n" +
-              queue.node.createProgressBar() ?? "finished"
+              '\n' +
+              queue.node.createProgressBar() ?? 'finished'
           );
         }, 1000);
       }
       break;
-    case "next":
+    case 'next':
       commandInteraction.deleteReply();
       queue.node.skip();
       break;
-    case "disconnect":
+    case 'disconnect':
       commandInteraction.deleteReply();
       queue.delete();
       queue.connection?.disconnect();
       break;
-    case "repeat":
+    case 'repeat':
       commandInteraction.deleteReply();
       if (queue.node.queue.repeatMode == QueueRepeatMode.OFF) {
         queue.node.queue.setRepeatMode(QueueRepeatMode.QUEUE);
@@ -160,18 +160,18 @@ client.on("interactionCreate", async (interaction) => {
         queue.node.queue.setRepeatMode(QueueRepeatMode.OFF);
       }
       break;
-    case "list":
+    case 'list':
       let titles = queue.tracks
         .map((track, index) => {
           let title = track.title.trim();
           return (
-            (index + 1).toString().padStart(2, " ") +
-            " : " +
+            (index + 1).toString().padStart(2, ' ') +
+            ' : ' +
             title.substring(0, 18)
           );
         })
-        .join("\n");
-      commandInteraction.editReply("```" + titles + "```");
+        .join('\n');
+      commandInteraction.editReply('```' + titles + '```');
   }
 });
 
